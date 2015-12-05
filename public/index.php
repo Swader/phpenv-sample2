b14a866acc394a32ed28402070cdf4e2aeb7f046
Bruno Skvorc <bruno@skvorc.me>
2015-12-05 23:43:59 +0000

    Third push

--- public/index.php
+++ public/index.php
@@ -7,6 +7,12 @@ $database = new medoo([
     'database_file' => '../storage/database.db'
 ]);
 
+$comment = new SitePoint\Comment($database);
+$comment->setEmail('bruno@skvorc.me')
+    ->setName('Bruno Skvorc')
+    ->setComment('Hooray! Saving comments works!')
+    ->save();
+
 ?>
 <!doctype html>
 <html class="no-js" lang="">
